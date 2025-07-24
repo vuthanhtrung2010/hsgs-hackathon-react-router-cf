14ebf5eafca5c34fe2051538dd7591a76b55b351
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2025-07-24 00:02:28 +0000

    feat: add TextType component for animated text display in Home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import TextType from "~/components/TextType";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,7 +11,19 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="font-sans min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome</h1>
+      <h1 className="text-4xl font-bold">
+        <TextType
+          text={["Welcome to HSGS Hackathon 2025!", "This is made by Trung.", "Yikes"]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          deletingSpeed={50}
+          loop={true}
+          className="text-center text-blue-600"
+          showCursor={true}
+          cursorCharacter="_"
+          cursorClassName="text-blue-600"
+        />
+      </h1>
     </div>
   );
 }
